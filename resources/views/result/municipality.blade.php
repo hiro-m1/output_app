b4176b42210f87ac88e14047e0badde39ca7089f
廣沢 槙弘 <m61m47@gmail.com>
2022-01-21 19:53:14 +0900

    チェックボックスの選択範囲の修正

--- resources/views/result/municipality.blade.php
+++ resources/views/result/municipality.blade.php
@@ -26,9 +26,9 @@
                 <tbody>
                   @if (!empty($prefectures))
                   @foreach ($prefectures as $prefecture)
-                  <tr>
+                  <tr onclick="getElementById('checkbox{{ $prefecture->municipality_id  }}').click();">
                     <th scope="row" style="width:15%">
-                     <input type="checkbox" name="municipality[]" value="{{ $prefecture->prefecture_id }}_{{ $prefecture->municipality_id }}" aria-label="Checkbox for following text input" onClick="DisChecked();">
+                     <input type="checkbox" name="municipality[]" value="{{ $prefecture->prefecture_id }}_{{ $prefecture->municipality_id }}" aria-label="Checkbox for following text input" onClick="DisChecked();" id="checkbox{{ $prefecture->municipality_id }}">
                     </th>
                     <td>
                       {{ $prefecture->prefecture_name }}
--- resources/views/result/station.blade.php
+++ resources/views/result/station.blade.php
@@ -26,9 +26,9 @@
                 <tbody>
                   @if (!empty($routes))
                   @foreach ($routes as $route)
-                  <tr>
+                  <tr onclick="getElementById('checkbox{{ $route->station_id  }}').click();">
                     <th scope="row" style="width:15%">
-                     <input type="checkbox" name="station[]" value="{{ $route->route_id }}_{{ $route->station_id }}" aria-label="Checkbox for following text input" onClick="DisStationChecked();">
+                     <input type="checkbox" name="station[]" value="{{ $route->route_id }}_{{ $route->station_id }}" aria-label="Checkbox for following text input" onClick="DisStationChecked();" id="checkbox{{ $route->station_id }}">
                     </th>
                     <td>
                       {{ $route->route_name }}
@@ -41,9 +41,9 @@
                   @endif
                   @if (!empty($stations))
                   @foreach ($stations as $station)
-                  <tr>
+                  <tr onclick="getElementById('checkbox{{ $station->station_id  }}').click();">
                     <th scope="row" style="width:15%">
-                     <input type="checkbox" name="station[]" value="{{ $station->route_id }}_{{ $station->station_id }}" aria-label="Checkbox for following text input" onClick="DisStationChecked();">
+                     <input type="checkbox" name="station[]" value="{{ $station->route_id }}_{{ $station->station_id }}" aria-label="Checkbox for following text input" onClick="DisStationChecked();" id="checkbox{{ $station->station_id }}">
                     </th>
                     <td>
                       {{ $station->route_name }}
