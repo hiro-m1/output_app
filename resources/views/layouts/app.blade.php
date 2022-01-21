294563272c40ec8381d55cf5e3e9f35e3b3f0ffb
廣沢 槙弘 <m61m47@gmail.com>
2022-01-21 19:12:22 +0900

    JSの読み込み

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/all_check.js') }}" defer></script>
+    <script src="{{ asset('js/text_copy.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
